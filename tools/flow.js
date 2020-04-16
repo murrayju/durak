d514f01311648ef100fc397e926b0de05c7cfeb1
Justin Murray <justin@murrayju.com>
2020-04-15 20:24:08 -0600

    Faster flow check

--- tools/flow.js
+++ tools/flow.js
@@ -4,13 +4,18 @@ import { buildLog, spawn } from 'build-strap';
 // Normally handled by eslint, but we get more debug info when run separately
 export default async function flow(
   allBranches?: boolean = process.argv.includes('--show-all-branches'),
+  fullCheck?: boolean = process.argv.includes('--flow-full-check'),
 ) {
   if (process.argv.includes('--no-flow')) {
     buildLog('Skipping due to --no-flow');
     return;
   }
-  await spawn('flow', ['check', ...(allBranches ? ['--show-all-branches'] : [])], {
-    stdio: 'inherit',
-    shell: true,
-  });
+  await spawn(
+    'flow',
+    [fullCheck ? 'check' : 'status', ...(allBranches ? ['--show-all-branches'] : [])],
+    {
+      stdio: 'inherit',
+      shell: true,
+    },
+  );
 }
